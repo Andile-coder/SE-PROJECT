3d659a15504f2cb8403421db9a45b42cb37b7ee2
AndileMasela <maselaandile@gmail.com>
2022-09-12 04:04:06 +0200

    error desplay

--- src/Controllers/Booking.ts
+++ src/Controllers/Booking.ts
@@ -45,7 +45,7 @@ export class BookingController {
       pgClient.release();
       return response.status(200).json(movies);
     } catch (error) {
-      return next(APIError.internalError('Network Error: Failed to get movis'));
+      return next(APIError.internalError('Network Error: Failed to get movis', error));
     }
   }
 
